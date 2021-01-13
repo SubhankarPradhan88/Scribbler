1c483e3c081b134175b9c1ec341ad0057d878d5b
SubhankarPradhan88 <subhankarpradhan88@yahoo.com>
2021-01-14 03:22:00 +0530

    Handle Sign In, Sign Up and Create Post Modals

---
+++ scripts/common.js
@@ -0,0 +1,24 @@
+// Click handler for Sign Up link
+function signUpModal(e) {
+    e.preventDefault();   // To prevent further propagation of the current event
+    e.stopPropagation();  // To prevent the default action made by the browser on button click
+    document.getElementById('closeSignInModal').click();  // Trigger click to close the Sign In Modal
+    document.getElementById('signUpInModal').click();  // Trigger click to open the Sign Up Modal
+    return false;
+}
+// Check madatory fields validation for Sign Up action
+document.getElementById('signUpAction').addEventListener('click', function() {
+    if(!document.getElementById('name').value.trim() || 
+    !document.getElementById('userName').value.trim() || 
+    !document.getElementById('userPassword').value.trim() ||
+    !document.getElementById('confirmPassword').value.trim()) {
+        return false;
+    }
+});
+// Check madatory fields validation for Sign In action
+document.getElementById('signInAction').addEventListener('click', function() {
+    if(!document.getElementById('signInName').value.trim() || 
+    !document.getElementById('signInPassword').value.trim()) {
+        return false;
+    }
+});
